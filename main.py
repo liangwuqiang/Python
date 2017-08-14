66409fbd781de7bc36cdbba4c3b5d473aaf8f03f
liangwuqiang <l_wuqiang@126.com>
2017-08-15 07:57:39 +0800

    a

--- main.py
+++ main.py
@@ -1,7 +1,41 @@
-from urllib2 import Request, urlopen, URLError, HTTPError
+#!/usr/bin/env python
+# encoding: utf-8
 
-old_url = 'http://www.baidu.com'
-req = Request(old_url)
-response = urlopen(req)
-print 'Info():'
-print response.info()
+from docx import document
+# from docx import Document
+# from docx.shared import Inches
+
+
+def p(text):
+    print(type(text))
+    print(text)
+
+# document = Document()
+# p(document)
+#
+# document.add_heading('Document Title', 0)
+#
+# p = document.add_paragraph('A plain paragraph having some ')
+# p.add_run('bold').bold = True
+# p.add_run(' and some ')
+# p.add_run('italic.').italic = True
+#
+# document.add_heading('Heading, level 1', level=1)
+# document.add_paragraph('Intense quote', style='IntenseQuote')
+#
+# document.add_paragraph(
+#     'first item in unordered list', style='ListBullet'
+# )
+# document.add_paragraph(
+#     'first item in ordered list', style='ListNumber'
+# )
+#
+#
+# table = document.add_table(rows=1, cols=3)
+# hdr_cells = table.rows[0].cells
+# hdr_cells[0].text = 'Qty'
+# hdr_cells[1].text = 'Id'
+# hdr_cells[2].text = 'Desc'
+# document.add_page_break()
+#
+# document.save('demo.docx')
