ec35839cc99b0c81dc361446cdb07b9f62cc7543
liangwuqiang <l_wuqiang@126.com>
2017-08-23 17:46:54 +0800

    a

---
+++ MyDict/transpy.py
@@ -0,0 +1,111 @@
+# coding=utf-8
+"""
+将一个目录下的所有.py文件中的注释部分添加中文注释
+使用: 修改目录变量rootdir
+"""
+import os
+import logging
+import time
+from urllib.request import quote, urlopen
+import json
+import re
+
+rootdir = '/home/lwq/Python/MyDict/test/lwq'
+regular = r'# (.*)'
+re_comment = re.compile(regular)
+regular = r'"""(.*?)"""'
+re_oneline = re.compile(regular)
+regular = r'(.*?)"""(.*)'
+re_explain = re.compile(regular)
+
+
+def translate(message):
+    return '(翻译略)'
+    api = 'http://fanyi.youdao.com/openapi.do' \
+          '?keyfrom=wufeifei&key=716426270&type=data&doctype=json&version=1.1&q='
+    # api = api + quote(message.encode('utf-8'))
+    api = api + quote(message)
+    # print(api, type(api))
+    try:
+        content = urlopen(api).read()
+        content = json.loads(content.decode('utf-8'))
+    except Exception as e:
+        print('出错: ', e)
+        return ''
+    else:
+        time.sleep(1)
+        return content['translation'][0]
+
+
+def deal(line, message):
+    # newline = line
+    if not line:
+        message = translate(message)
+        newline = line.rstrip() + ' #--> ' + message + '\n'
+        # logging.debug(line)
+        return newline
+    return line
+
+
+def handle(lines):
+    content = []
+    tag = [False]
+    for line in lines:
+        newline = line
+        if not newline:
+            newline = '\n'
+        elif re_comment.findall(line):
+            message = re_comment.search(line).group(1)
+            newline = deal(line, message)
+            # print(newline.rstrip())
+        elif re_oneline.findall(line):
+            message = re_oneline.search(line).group(1)
+            newline = deal(line, message)
+            # print(newline.rstrip())
+        elif tag[0] is True and line.find('"""') == -1:
+            message = line.strip()
+            newline = deal(line, message)
+            print(newline.rstrip())
+        elif len(re_explain.findall(line)) == 1:
+            if tag[0] is False:
+                tag[0] = True
+                message = re_explain.search(line).group(2)  # 右边部分
+                if message:
+                    newline = deal(line, message)
+            else:
+                tag[0] = False
+                message = re_explain.search(line).group(1)  # 左边部分
+                if message:
+                    newline = deal(line, message)
+        content.append(newline)
+    return ''.join(content)
+
+
+def openfile(path):
+    try:
+        with open(path) as f:
+            lines = f.readlines()
+    except Exception as e:
+        message = path + '\n' + str(e)
+        logging.error(message)
+    else:
+        return lines
+
+
+def main():
+    for parent, dirnames, filenames in os.walk(rootdir):
+        for filename in filenames:
+            path = os.path.join(parent, filename)
+            ext = os.path.splitext(path)[-1]
+            if ext == '.py':
+                lines = openfile(path) if openfile(path) else ''
+                if len(lines) > 0:
+                    content = handle(lines)
+                    filename = os.path.splitext(path)[0] + '_new.txt'
+                    with open(filename, 'w') as f:
+                        f.write(content)
+            # time.sleep(1)
+
+
+if __name__ == '__main__':
+    main()
--- 学习笔记/python注意事项.md
+++ 学习笔记/python注意事项.md
@@ -10,3 +10,10 @@ from urllib import request
 filename = 'file://' + request.pathname2url(r'd:\windows\temp.html')
 response = request.urlopen(filename)
 html = response.read().decode('utf-8')
+
+## 导入模块和引用模块的不同?
+import os.path
+for parent, dirnames, filenames in os.walk(rootdir):
+所以直接使用import os就可? 反正在os.path中也得带os,不能单独使用path
+
+
