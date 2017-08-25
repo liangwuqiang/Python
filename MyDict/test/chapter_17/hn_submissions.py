3b89f640cf14c836224e49046a4fd4af384ebb17
liangwuqiang <l_wuqiang@126.com>
2017-08-25 17:57:58 +0800

    a

--- MyDict/test/chapter_17/hn_submissions.py
+++ MyDict/test/chapter_17/hn_submissions.py
@@ -10,10 +10,10 @@ print("Status code:", r.status_code)
 # Process information about each submission.
 submission_ids = r.json()
 submission_dicts = []
-for submission_id in submission_ids[:30]:
+for submission_id in submission_ids[:3]:
     # Make a separate API call for each submission.
     url = ('https://hacker-news.firebaseio.com/v0/item/' +
-            str(submission_id) + '.json')
+           str(submission_id) + '.json')
     submission_r = requests.get(url)
     print(submission_r.status_code)
     response_dict = submission_r.json()
@@ -26,9 +26,13 @@ for submission_id in submission_ids[:30]:
     submission_dicts.append(submission_dict)
     
 submission_dicts = sorted(submission_dicts, key=itemgetter('comments'),
-                            reverse=True)
+                          reverse=True)
 
-for submission_dict in submission_dicts:
-    print("\nTitle:", submission_dict['title'])
-    print("Discussion link:", submission_dict['link'])
-    print("Comments:", submission_dict['comments'])
+with open('hn.txt', 'w') as f:
+    for submission_dict in submission_dicts:
+        print("\nTitle:", submission_dict['title'])
+        print("Discussion link:", submission_dict['link'])
+        print("Comments:", submission_dict['comments'])
+        f.write(submission_dict['title'] + '\n')
+        f.write(submission_dict['link'] + '\n')
+        f.write(str(submission_dict['comments']) + '\n\n')
